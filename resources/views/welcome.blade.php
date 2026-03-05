4a21464112f0f6a3886f8b6f06446eb6cd514d48
Asyraf Ismail <nikasyraf268@gmail.com>
2026-03-05 10:34:06 +0800

    feat: add tailwind and css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employee Network</title>
+    @vite('resources/css/app.css');
 </head>
-<body>
+<body class="text-center px-8 py-12">
     <h1>Welcome to the Employee Network</h1>
     <p>Click the button below to view the list of employees.</p>
-    <a href="/employees" class="btn">Find Employees</a>
+    <a href="/employees" class="btn mt-4 inline-block">Find Employees</a>
 </body>
 </html>
